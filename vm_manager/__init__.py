9b9ed0e0f386a46b5c047d56f5abc806401de458
Nobita Cloud <nn8109239@gmail.com>
2026-06-11 01:42:13 +0530

    Update __init__.py

--- vm_manager/__init__.py
+++ vm_manager/__init__.py
@@ -2,4 +2,4 @@ from .vm_manager import VMManager, VMConfig, Colors
 from .config import VM_DIR, WEB_DIR, PIDS_DIR, CPU_MODELS, OS_IMAGES
 
 __version__ = "2.0.0"
-__author__ = "Hopingboyz"
+__author__ = "Nobita"
